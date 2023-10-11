e29be01595c663536d13dc0be295bec1b6cae6ae
hubcad25 <hubertcadieux@gmail.com>
2023-10-10 23:14:38 -0400

    add fct load_variable

---
+++ R/merging.R
@@ -0,0 +1,26 @@
+#' Load variable from file
+#'
+#'This function takes a file and a variable name and returns the vector
+#'containing the variable values.
+#'
+#' @param file A string containing the path to a file.
+#' @param raw_variable_name A string containing the name of the variable.
+#'
+#' @return A vector with the same number of rows as the file containing the variable values
+#' @export
+#'
+#' @examples
+#' load_variable(file = "data.csv", variable_name = "var332") ## should return the vector containing the values of var332.
+load_variable <- function(file, variable_name){
+  ## get file extension
+  ext <- tools::file_ext(file)
+  ## load data according to extension
+  if (ext == "csv"){
+    data <- read.csv(file, encoding = "UTF-8")
+  } else if (ext == "xlsx"){
+    data <- readxl::read_xlsx(file)
+  } else if (ext == "sav"){
+    data <- haven::read_sav(file)
+  }
+  return(data)
+}
