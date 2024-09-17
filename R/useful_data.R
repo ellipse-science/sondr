8c2389b0aa37ef744b32f439f83ca4dcd1c41d9f
hubcad25 <hubertcadieux@gmail.com>
2024-09-16 23:44:50 -0400

    add us_counties_data

---
+++ R/useful_data.R
@@ -0,0 +1,18 @@
+#' US Counties Data
+#'
+#' A dataset containing information about US counties, such as area and other attributes.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{county_fips}{FIPS code of the county}
+#'   \item{population_density}{Population density in square miles}
+#'   \item{vote_projection_democrat}{ARIMA projection of democrat vote share in 2024}
+#' }
+#'
+#' @usage data(us_counties_data)
+#'
+#' @docType data
+#' @name us_counties_data
+#' @keywords datasets
+#'
+"us_counties_data"
