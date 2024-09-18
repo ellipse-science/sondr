0af30e89d3bb33b980a5f156befc4dfd9828b818
hubcad25 <hubertcadieux@gmail.com>
2024-09-17 20:26:35 -0400

    update us_counties_data

--- R/useful_data.R
+++ R/useful_data.R
@@ -6,7 +6,7 @@
 #' \describe{
 #'   \item{county_fips}{FIPS code of the county}
 #'   \item{population_density}{Population density in square miles}
-#'   \item{vote_projection_democrat}{ARIMA projection of democrat vote share in 2024}
+#'   \item{projection_vote_democrat}{ARIMA projection of democrat vote share in 2024}
 #' }
 #'
 #' @usage data(us_counties_data)
@@ -15,4 +15,3 @@
 #' @name us_counties_data
 #' @keywords datasets
 #'
-"us_counties_data"
