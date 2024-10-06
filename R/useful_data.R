f4dfb0307350c940158e2833d133dce3bcb7a66b
hubcad25 <hubertcadieux@gmail.com>
2024-10-06 18:13:58 -0400

    add us_counties_map.rda

--- R/useful_data.R
+++ R/useful_data.R
@@ -15,3 +15,22 @@
 #' @name us_counties_data
 #' @keywords datasets
 #'
+
+#' US Counties Map Data
+#'
+#' A dataset containing geographic coordinates for the boundaries of US counties.
+#'
+#' This dataset provides the latitude and longitude points defining the boundaries of counties across the United States, identified by their respective FIPS codes.
+#'
+#' @format A tibble with 228,454 rows and 3 variables:
+#' \describe{
+#'   \item{long}{Numeric, the longitude of a point on the county boundary.}
+#'   \item{lat}{Numeric, the latitude of a point on the county boundary.}
+#'   \item{county_fips}{Character, the FIPS code of the county corresponding to the point.}
+#' }
+#'
+#' @usage data(us_counties_map)
+#' @export
+#' @docType data
+#' @keywords datasets
+#' @name us_counties_map
